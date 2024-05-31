149134714460481ff7fdf2c11cdded1d884def0d
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-05-31 16:38:11 +0200

    Adds unit test mocks

---
+++ src/mocks/response.mock.js
@@ -0,0 +1,9 @@
+import { userMock } from "./user.mock.js";
+
+export const responseMock = {
+  name: userMock.name,
+  email: userMock.email,
+  avatar: userMock.avatar,
+  banner: userMock.banner,
+  accessToken: userMock.accessToken,
+};
---
+++ src/mocks/user.mock.js
@@ -0,0 +1,8 @@
+export const userMock = {
+  name: "testuser",
+  email: "test@stud.noroff.no",
+  password: "testpassword123",
+  avatar: "",
+  banner: "",
+  accessToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9....",
+};
